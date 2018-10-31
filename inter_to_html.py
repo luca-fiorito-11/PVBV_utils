15213020fd9fc95232571dc629bab354e4d05ec9
Luca Fiorito <luca.fiorito@oecd.org>
2018-10-31 16:21:16 +0100

    update

--- inter_to_html.py
+++ inter_to_html.py
@@ -2,11 +2,10 @@ import sys
 import pandas as pd
 
 widths=(4,4,5,6,3,12,12,12,11,8,13,12,12)
-skiprows=list(range(0,26)) + [27]
 df = pd.read_fwf(
          sys.argv[1],
          widths=widths,
-         skiprows=skiprows,
+         skiprows=[1],
          ).fillna(0)
 table = df.to_html(index=False)
 body = """---
