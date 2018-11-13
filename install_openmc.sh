09978844c6eaacc2a7092b47f366aedc24ee8a18
Luca Fiorito <luca.fiorito@oecd.org>
2018-11-13 16:59:40 +0100

    updated .travis.yml

--- install_openmc.sh
+++ install_openmc.sh
@@ -1,8 +1,5 @@
-# install prerequisites
-sudo apt install gfortran g++ cmake libhdf5-dev
-
 # Download the source code
 git clone https://github.com/mit-crpg/openmc.git
 
 # install on local python
-cd openmc && pip setup.py
+cd openmc && python setup.py install
