1aa6a038afd678f941ff1cdbb9748b9f8c89f5be
Luca Fiorito <luca.fiorito@oecd.org>
2018-11-13 17:07:41 +0100

    update

--- openmc_to_html.py
+++ openmc_to_html.py
@@ -8,8 +8,8 @@ del info['identifier']
 info["library"] = info["library"][0] + "-" + str(info["library"][1]) + "." + str(info["library"][2])
 info = dict(info, **EV.target)
 # Add links to other codes' outputs
-info["NJOY output"]<div><a href="njoy_output.html">here</a></div>
-info["INTER output"]<div><a href="inter_output.html">here</a></div>
+info["NJOY output"] = '<div><a href="njoy_output.html">here</a></div>'
+info["INTER output"] = '<div><a href="inter_output.html">here</a></div>'
 name = info.pop("zsymam")
 
 df = pd.Series(info).rename(name.strip()).to_frame()
