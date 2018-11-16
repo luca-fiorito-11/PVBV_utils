5b8ce686549adb8ad9697977f4c3ac34c5e20986
Luca Fiorito <luca.fiorito@oecd.org>
2018-11-16 18:11:20 +0100

    update

--- install_fizcon.sh
+++ install_fizcon.sh
@@ -1,4 +1,4 @@
 apt-get gfortran-5
 
-curl -o fizcon.f90 "https://www-nds.iaea.org/public/endf/utility/bin/fizcon.f" -k
-${FC} -o fizcon fizcon.f90
+curl -O "https://www-nds.iaea.org/public/endf/utility/fortran/fizcon.f"
+${FC} -o fizcon fizcon.f
