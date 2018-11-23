c94466ebe2bae08c7b1d39dad82a1bba26f9de8c
Luca Fiorito <luca.fiorito@oecd.org>
2018-11-23 11:44:05 +0100

    update

--- init_repo_lib.sh
+++ init_repo_lib.sh
@@ -1,50 +1,31 @@
-file=$1
-repo_name=$2
-filename=$(basename $file)
 user="luca-fiorito-11"
-tag="v4.0T0"
-token=$(cat ~/.github_token)
-mkdir -p $repo_name
-cp $file ${repo_name}/
-cd $repo_name
+repo="jeff-dev"
+branch="master"
 
-# initialize git repo
-git init
+# Clone repository and submodules
+git clone --recurse-submodules -j8 https://github.com/${user}/${repo}.git
+cd ${repo}
 
-# Initial source import
-git add .
-git commit -m "Initial import"
+# Switch to desired branch
+git checkout ${branch}
 
-# Create the 'publish' branch
-git checkout --orphan gh-pages
-rm -rf *
-git rm --cached *
-touch .keep
-git add --all .
-git commit -m "clean publish branch"
-
-# Push to remote
-git checkout master
-git config credential.helper store
-curl -i -d "{\"name\" : \"${repo_name}\"}" https://api.github.com/user/repos -k -u ${user}:${token}
-git remote add origin https://github.com/${user}/${repo_name}.git
-git push -u origin master
-git checkout gh-pages
-git push -u origin gh-pages
-git checkout master
+# Update submodules
+while IFS=" " read -r submodule tag remainder
+do
+  git submodule add https://github.com/${user}/${submodule}.git
+  (cd ${submodule} && git checkout $tag)
+  git add ${submodule}
+done < $1
+git commit -m "moved submodules to requested tags"
 
 # Include travis CD/CI
 git clone https://github.com/luca-fiorito-11/PVBV_utils.git
-sed "s/filename_placeholder/$filename/" <PVBV_utils/travis_eval_template.yml >.travis.yml
-git add .travis.yml
+cp PVBV_utils/template_travis_lib.yml .travis.yml
+while IFS=" " read -r submodule tag remainder
+do
+   echo "curl -o \${ACEDIR}/${submodule}.ace https://github.com/${user}/${submodule}/releases/download/${tag}/${submodule}.jeff33.ace -k -L"
+done < $1 > get_releases.sh
+git add get_releases.sh .travis.yml
 git commit -m "add travis CI"
-git push origin master
 
-# Enable travis-CI with TRVAIS-CI Client API (must be logged in)
-travis enable --org -I -r ${user}/${repo_name}
-travis env set GITHUB_TOKEN $token --private -I -r ${user}/${repo_name}
-
-# Tag reference release
-git tag ${tag}
-git push origin ${tag}
 cd ..
