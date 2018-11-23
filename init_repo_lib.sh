4f660b8d50b06f5b3bcb6bcc4a6787f37f5116b6
Luca Fiorito <luca.fiorito@oecd.org>
2018-11-23 14:14:15 +0100

    upadte

--- init_repo_lib.sh
+++ init_repo_lib.sh
@@ -1,30 +1,33 @@
+list=$1
 user="luca-fiorito-11"
 repo="jeff-dev"
 branch="master"
+
+set -e
 
 # Clone repository and submodules
 git clone --recurse-submodules -j8 https://github.com/${user}/${repo}.git
 cd ${repo}
 
 # Switch to desired branch
-git checkout ${branch}
+[[ ${branch} != "master" ]] && git checkout -b ${branch}
 
 # Update submodules
 while IFS=" " read -r submodule tag remainder
 do
-  git submodule add https://github.com/${user}/${submodule}.git
-  (cd ${submodule} && git checkout $tag)
+  git submodule status | awk '{ print $2 }' | grep -q ${submodule} || git submodule add https://github.com/${user}/${submodule}
+  (cd ${submodule} && git checkout ${tag})
   git add ${submodule}
-done < $1
+done < ${list}
 git commit -m "moved submodules to requested tags"
 
 # Include travis CD/CI
 git clone https://github.com/luca-fiorito-11/PVBV_utils.git
-cp PVBV_utils/template_travis_lib.yml .travis.yml
+cp PVBV_utils/travis_lib_template.yml .travis.yml
 while IFS=" " read -r submodule tag remainder
 do
    echo "curl -o \${ACEDIR}/${submodule}.ace https://github.com/${user}/${submodule}/releases/download/${tag}/${submodule}.jeff33.ace -k -L"
-done < $1 > get_releases.sh
+done < ${list} > get_releases.sh
 git add get_releases.sh .travis.yml
 git commit -m "add travis CI"
 
